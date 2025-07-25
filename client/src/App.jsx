5714958967e835d0b9d9551588ad18f50ed50746
panda <rabbyrezbeegmail.com>
2025-07-25 17:53:58 +0200

    added dashboard & add room pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
 import HotelReg from "./components/HotelReg";
 import Layout from "./pages/hotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import AddRoom from "./pages/hotelOwner/AddRoom";
+import ListRoom from "./pages/hotelOwner/ListRoom";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -23,10 +26,15 @@ const App = () => {
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
           {/* ---owner routes */}
-          <Route path="/owner" element={<Layout />}></Route>
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+          </Route>
         </Routes>
       </div>
       <Footer />
+    
     </div>
   );
 };
